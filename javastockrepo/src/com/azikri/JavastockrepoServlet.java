1250f0bab1e7af03dffe15a09b06d80de5e05267
Roy Azikri <royazikri@gmail.com>
2015-04-03 17:37:17 +0300

    ex 02

--- javastockrepo/src/com/azikri/JavastockrepoServlet.java
+++ javastockrepo/src/com/azikri/JavastockrepoServlet.java
@@ -5,9 +5,19 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class JavastockrepoServlet extends HttpServlet {
+	int num1=4;
+	int num2=3;
+	int num3=7;
+	int result= (num1+num2)*num3;
+	int var1= num1+num2;
+	int var2=num3;
+	String resultStr = new String("<h1>Result of"+var1+"*"+var2+"="+result+"</h1>");
+	
+	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+		
+		resp.getWriter().println("Result of (num1+num2) * num3 is: result");
 	}
 }
