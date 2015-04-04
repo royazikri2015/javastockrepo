645604932d69089b2035fa41ab43b1140df65106
Roy Azikri <royazikri@gmail.com>
2015-04-04 15:42:29 +0300

    ex2

--- javastockrepo/src/com/azikri/JavastockrepoServlet.java
+++ javastockrepo/src/com/azikri/JavastockrepoServlet.java
@@ -5,19 +5,16 @@ import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class JavastockrepoServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/html");
 	int num1=4;
 	int num2=3;
 	int num3=7;
 	int result = (num1+num2)*num3;
 	
-	
-	
-	
-	public void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws IOException {
-		resp.setContentType("text/html");
-		String resultStr = new String( "<h1>Result of"+num1+"*"+num2+"="+result+"</h1>");
-				resp.getWriter().println( resultStr );
+	String resultStr = new String( "<h1>Result of"+num1+"*"+num2+"="+result+"</h1>");
+	resp.getWriter().println( resultStr );
 		
 	
 	}
